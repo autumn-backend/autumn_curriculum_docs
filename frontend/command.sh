9cdad1f84b927a65773fba0f3c48254bb8347e43
ryosuke1202 <ryosuke12021@gmail.com>
2022-09-11 13:39:39 +0900

    コマンド自動実行用のシェルスクリプト追加

---
+++ frontend/command.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+gcloud config set project gcloud-project-358506
+
+gcloud auth configure-docker
+
+docker build -t "gcr.io/gcloud-project-358506/zenn-preview" .
+
+docker push "gcr.io/gcloud-project-358506/zenn-preview"
+
+DATE=`date '+%Y%m%d'`
+
+service_name=autumn-frontend-$DATE
+
+gcloud run deploy "$service_name" \
+  --image "gcr.io/gcloud-project-358506/zenn-preview" \
+  --port 8000 \
+  --platform managed \
+  --allow-unauthenticated \
+  --region asia-northeast1
